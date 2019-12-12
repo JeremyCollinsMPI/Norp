0f84227b89aed3034386dee5b54c7752b8c4f54d
Jeremy Collins <jeremyccollins@gmail.com>
2019-12-12 11:33:34 +0800

    Docker corrections

--- norp/sql.sh
+++ norp/sql.sh
@@ -1,2 +1,2 @@
 mysql_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mysql)
-docker run --rm -it -p 80:80 -e mysql_ip=$mysql_ip --name norp norp /bin/bash
+docker run --rm -it -p 80:80 -e mysql_ip=$mysql_ip --name norp jeremycollinsmpi/norp /bin/bash
