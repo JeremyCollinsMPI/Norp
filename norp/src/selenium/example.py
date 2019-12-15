ac060082ae5d690b0ed5c6c08bd23515cd8a7b8a
Jeremy Collins <jeremycollins@robotdatasmbp2.lan>
2019-12-15 13:28:35 +0800

    correction

--- norp/src/selenium/example.py
+++ norp/src/selenium/example.py
@@ -3,11 +3,12 @@ from time import sleep
 import os
 
 
-
+files_already_done = os.listdir('../images/')
 
 embeddings = open('embeddings.txt','r').readlines()
 embeddings = [x.strip('\n') for x in embeddings]
 embeddings = [x for x in embeddings if not x == '']
+print(len(embeddings))
 
 def get_url_and_username(embedding):
   string = embedding.split(">A post shared by ")[1]
@@ -26,6 +27,15 @@ def get_url_and_username(embedding):
 for embedding in embeddings:
   print get_url_and_username(embedding)
 
+def make_filename(username):
+  return username + '_1'
+
+def already_done(username):
+  if make_filename(username) + '.png' in files_already_done:
+    return True
+  else:
+    return False
+  
 
 browser = webdriver.Chrome()
 browser.get('chrome://settings/')
@@ -38,13 +48,17 @@ def take_screenshot(url, filename, close_login=False):
     browser.execute_script("document.querySelectorAll('button.dCJp8.afkep.xqRnw')[0].click();")
   sleep(5)
   os.system('screencapture -R200,300,500,600 ../images/' + filename + '.png')
-  
+
+counter = 0 
 for i in range(len(embeddings)):
   close_login = False
-  if i == 0:
+  if counter == 0:
     close_login = True
   embedding = embeddings[i]
   url, username = get_url_and_username(embedding)
-  take_screenshot(url, username + '_1', close_login=close_login)
+  if not already_done(username):
+    print username
+    take_screenshot(url, username + '_1', close_login=close_login)
+    counter = counter + 1
 browser.close()
 
